36658029a7df8d6104b55e7326061d8d54594c46
DZ-tt2195 <tt2195@nyu.edu>
2023-02-03 19:01:15 -0500

    fixed some bugs

--- Assets/Scripts/CompassRotate.cs
+++ Assets/Scripts/CompassRotate.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class CompassRotate : MonoBehaviour
 {
-    public Transform currentobjective;
+    public Transform north;
     public Transform compass2D;
 
     void Update()
     {
-        transform.LookAt(currentobjective);
-        float change = transform.localRotation.y - compass2D.transform.localRotation.z;
+        transform.LookAt(north);
+        float change = (transform.localRotation.y - compass2D.transform.localRotation.z);
         compass2D.transform.Rotate(0, 0, change, Space.Self);
 
     }
