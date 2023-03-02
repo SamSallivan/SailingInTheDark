2a470fb0133449bc4e63af57063292824ee0ae1c
tinybitofheaven <abc9295@nyu.edu>
2023-03-01 20:28:46 -0500

    enemy layermask fix
    changed physics matrix
    changed dockingTrigger layer mask

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -7,32 +7,33 @@ public class EnemyMovement : MonoBehaviour
 {
     private GameObject boat;
     private Transform boatTransform;
+    private Transform target; //usually set to boat, can set it to other things if AI gets more complex
+    public LayerMask boatMask;
+    public LayerMask obstacleMask;
 
+    //stats for AI
+    [SerializeField] private Vector3 offset;
     private float movementSpeed = 3f;
     private float rotationDamp = 5f;
     public float raycastDistance = 5f;
     public float raycastRadius = 2.5f;
 
-    private Transform target;
-
-    public LayerMask boatMask;
-    public LayerMask obstacleMask;
-
-    public Vector3 offset;
-
-    public bool canAttack = false;
+    //attack variables
     private float maxAttackCooldown = 5f; //seconds
-    public float curAttackCooldown = 5f; //seconds
+    public float curAttackCooldown; //seconds
     private int attackDamage = 100;
+    private int attackRange = 2;
 
     private void Awake()
     {
+        curAttackCooldown = maxAttackCooldown;
         boat = GameObject.Find("Boat");
         boatTransform = boat.transform;
 
         target = boatTransform;
     }
 
+    //STATE MACHINE LOGIC
     //if light: lose meter
     //if meter is 0, run - dip under water
     //if too far, despawn
@@ -60,11 +61,16 @@ public class EnemyMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Collider[] hitColliders = Physics.OverlapBox(transform.position, transform.localScale * 2, transform.rotation, boatMask, QueryTriggerInteraction.Collide);
+        Attack();
+    }
 
+    private void Attack()
+    {
+        //check if boat is in attack range
+        Collider[] hitColliders = Physics.OverlapBox(transform.position, transform.localScale * attackRange, transform.rotation, boatMask, QueryTriggerInteraction.Collide);
         if (hitColliders.Length > 0)
         {
-            // Debug.Log("HAS HIT");
+            //attack if boat is detected and enemy attack is off cooldown, then reset cooldown
             if (curAttackCooldown <= 0)
             {
                 BoatController.instance.TakeDamage(attackDamage);
@@ -72,15 +78,18 @@ public class EnemyMovement : MonoBehaviour
             }
             else
             {
+                //cooldown timer
                 curAttackCooldown -= Time.deltaTime;
             }
         }
         else
         {
+            //if boat is not in range, reset cooldown timer
             curAttackCooldown = maxAttackCooldown;
         }
     }
 
+    //turn towards boat position
     private void Turn()
     {
         Vector3 pos = target.position - transform.position;
@@ -88,33 +97,38 @@ public class EnemyMovement : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationDamp * Time.deltaTime);
     }
 
+    //move forward based on forward direction
     private void Move()
     {
         Vector3 pos = transform.position + (transform.forward * movementSpeed * Time.deltaTime);
-        pos.y = 0.3f;
+        pos.y = 0.3f; //clamp to sea level
         transform.position = pos;
     }
 
+    //enemy AI movement
     private void PathFinding()
     {
         RaycastHit hit;
-        offset = Vector3.zero;
+        offset = Vector3.zero; //vector to store turn angle
 
+        //look for obstacle
         bool ray = Physics.SphereCast(transform.position, raycastRadius, transform.forward, out hit, raycastDistance, obstacleMask);
         if (ray)
         {
+            //get angle between the normal vector of the obstacle and the direction the enemy is facing to determine
+            //which way and how much to turn
             float angle = Vector3.Angle(hit.normal, -transform.forward);
-
             if (-hit.normal.x > transform.forward.x)
             {
-                offset = new Vector3(0f, -(90 - angle), 0);
+                offset = new Vector3(0f, -(90 - angle), 0f);
             }
             else
             {
-                offset = new Vector3(0f, 90 - angle, 0);
+                offset = new Vector3(0f, 90 - angle, 0f);
             }
         }
 
+        //if turning is required, rotate the enemy to face the turn direction, otherwise turn towards target
         if (offset != Vector3.zero)
         {
             transform.Rotate(offset * Time.deltaTime);
@@ -127,8 +141,13 @@ public class EnemyMovement : MonoBehaviour
         Move();
     }
 
+
     private void OnDrawGizmos()
     {
+        Color color = Color.red;
+        color.a = 0.2f;
+        Gizmos.color = color;
+
         Gizmos.DrawSphere(transform.position + transform.forward * raycastDistance, raycastRadius);
     }
 }
