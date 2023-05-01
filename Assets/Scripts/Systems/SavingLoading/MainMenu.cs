2068b5f755b31c8a741c2b0e8f1def44801d6806
DZ-tt2195 <tt2195@nyu.edu>
2023-05-01 12:03:38 -0400

    fixed title screen

--- Assets/Scripts/Systems/SavingLoading/MainMenu.cs
+++ Assets/Scripts/Systems/SavingLoading/MainMenu.cs
@@ -9,11 +9,15 @@ using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+    [Foldout("Main Menu", true)]
     public TMP_Text warning;
+    public List<Button> settingsToggle;
     public Button newGame;
     public Button continueGame;
     public Button exit;
 
+    [Foldout("Settings", true)]
+    public GameObject settingsPage;
     public Toggle fullScreen;
     public Slider soundLoudness;
     public AudioMixer mixer;
@@ -33,6 +37,10 @@ public class MainMenu : MonoBehaviour
         newGame.onClick.AddListener(Begin);
         continueGame.onClick.AddListener(Continue);
         exit.onClick.AddListener(ExitGame);
+
+        settingsPage.SetActive(false);
+        for (int i = 0; i < settingsToggle.Count; i++)
+            settingsToggle[i].onClick.AddListener(Toggle);
 
         fullScreen.isOn = Screen.fullScreen;
         fullScreen.onValueChanged.AddListener(delegate { Screensize(); });
@@ -59,6 +67,11 @@ public class MainMenu : MonoBehaviour
             UnityEditor.EditorApplication.isPlaying = false;
         #endif
             Application.Quit();
+    }
+
+    public void Toggle()
+    {
+        settingsPage.SetActive(!settingsPage.activeSelf);
     }
 
     public void Screensize()
