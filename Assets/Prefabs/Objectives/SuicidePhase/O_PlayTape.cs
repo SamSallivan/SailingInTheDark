b6d8348934296992441917211b942127ccf8302c
tinybitofheaven <abc9295@nyu.edu>
2023-05-05 22:41:04 -0400

    added final trigger for lotus island

--- Assets/Prefabs/Objectives/SuicidePhase/O_PlayTape.cs
+++ Assets/Prefabs/Objectives/SuicidePhase/O_PlayTape.cs
@@ -8,6 +8,7 @@ using Unity.VisualScripting;
 public class O_PlayTape : Objective
 {
     public string requiredRecordingName;
+    public bool enableEndCutscene; //THIS IS ONLY FOR TAPE 4
 
     public void OnEnable()
     {
@@ -20,12 +21,16 @@ public class O_PlayTape : Objective
         RecordingManager.OnRecordingStart -= OnRecordingStart;
         RecordingManager.OnRecordingEnd -= OnRecordingEnd;
     }
-    
+
     public void OnRecordingStart()
     {
         if (CheckRecordingName())
         {
             //Finish();
+            if (enableEndCutscene)
+            {
+                RecorderController.hasHeardLastTape = true;
+            }
 
             //ObjectiveManager.instance.ObjectiveList.Remove(this);
             this.transform.GetChild(0).GetComponent<Image>().DOFade(1, 0.75f).OnComplete(() => CompleteFinished());
